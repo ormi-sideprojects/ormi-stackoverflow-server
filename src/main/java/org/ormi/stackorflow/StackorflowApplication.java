94ec725aca7d901732589b720e52f39ae1cf04a6
devdynam0507 <wsnam0507@naver.com>
2024-05-28 21:54:26 +0900

    chore: initial commit

---
+++ src/main/java/org/ormi/stackorflow/StackorflowApplication.java
@@ -0,0 +1,13 @@
+package org.ormi.stackorflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StackorflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StackorflowApplication.class, args);
+	}
+
+}
