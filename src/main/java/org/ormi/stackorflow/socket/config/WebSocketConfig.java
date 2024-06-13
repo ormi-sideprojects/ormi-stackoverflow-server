b2b781e1fca63a1040865389297af39e588f88d7
C0904 <cabinkid07@gmail.com>
2024-06-13 21:40:22 +0900

    refactor: 웹소켓 라우팅 경로 수정 /pub, /sub

--- src/main/java/org/ormi/stackorflow/socket/config/WebSocketConfig.java
+++ src/main/java/org/ormi/stackorflow/socket/config/WebSocketConfig.java
@@ -1,4 +1,4 @@
-package org.ormi.stackorflow.webscoket.config;
+package org.ormi.stackorflow.socket.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
@@ -7,8 +7,9 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 /**
- * 웹소켓 Configuration 파일입니다.
- * 
+ * 웹소켓 Configuration 파일입니다. 클라이언트는 "/ws" 경로로 연결을 시도할 수 있습니다. 서버는 "/topic" 접두사를 가진 목적지로 메시지를 전송할 수
+ * 있으며, 해당 메시지는 구독한 클라이언트에게 브로드캐스트됩니다.
+ *
  * @author 강채현
  */
 @Configuration
@@ -17,12 +18,13 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
   @Override
   public void configureMessageBroker(MessageBrokerRegistry registry) {
-    registry.enableSimpleBroker("/topic");
-    registry.setApplicationDestinationPrefixes("/app");
+    registry.enableSimpleBroker("/sub");
+    registry.setApplicationDestinationPrefixes("/pub");
   }
 
   @Override
   public void registerStompEndpoints(StompEndpointRegistry registry) {
-    registry.addEndpoint("/ws").withSockJS();
+    registry.addEndpoint("/ws").setAllowedOrigins("*");
   }
+
 }
