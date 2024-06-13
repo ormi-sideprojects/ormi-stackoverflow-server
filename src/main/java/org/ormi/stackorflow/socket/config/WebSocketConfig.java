390bf26a9534662bd097b18d94153a26d9df402a
C0904 <cabinkid07@gmail.com>
2024-06-13 21:41:20 +0900

    refactor: 주석 수정

--- src/main/java/org/ormi/stackorflow/socket/config/WebSocketConfig.java
+++ src/main/java/org/ormi/stackorflow/socket/config/WebSocketConfig.java
@@ -7,8 +7,7 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 /**
- * 웹소켓 Configuration 파일입니다. 클라이언트는 "/ws" 경로로 연결을 시도할 수 있습니다. 서버는 "/topic" 접두사를 가진 목적지로 메시지를 전송할 수
- * 있으며, 해당 메시지는 구독한 클라이언트에게 브로드캐스트됩니다.
+ * 웹소켓 Configuration 파일입니다. 클라이언트는 "/ws" 경로로 연결을 시도할 수 있습니다.
  *
  * @author 강채현
  */
