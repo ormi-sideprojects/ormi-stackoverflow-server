18dd71877ef152b43bccb31294cdbac864f3b9cf
JeongSeok <ljs0429777@gmail.com>
2024-06-21 00:00:41 +0900

    feat: 쿠키 인증으로 인한 스태프 도메인 로직 변경

--- src/main/java/org/ormi/stackorflow/api/controller/staff/StaffController.java
+++ src/main/java/org/ormi/stackorflow/api/controller/staff/StaffController.java
@@ -1,8 +1,10 @@
 package org.ormi.stackorflow.api.controller.staff;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.ormi.stackorflow.api.controller.staff.response.StaffResponse;
+import org.ormi.stackorflow.core.domain.common.CookieUtil;
 import org.ormi.stackorflow.core.domain.staff.StaffLogin;
 import org.ormi.stackorflow.core.domain.staff.StaffService;
 import org.ormi.stackorflow.infra.common.Responses;
@@ -20,13 +22,15 @@ public class StaffController {
 	private final StaffService staffService;
 
 	@PostMapping("/auth/staff/login")
-	public Responses<StaffResponse> staffLogin(@RequestBody StaffLogin staffLoginDto, HttpSession httpSession) {
+	public Responses<StaffResponse> staffLogin(@RequestBody StaffLogin staffLoginDto, HttpServletRequest request) {
 
-		// 요청으로 들어온 코드로 해당 스태프를 조회한다.
+		// 요청으로 들어온 인증코드로 해당 스태프를 조회한다.
 		StaffEntity beforeStaffLogin = staffService.getByCode(staffLoginDto.getCertificationCode());
 
-		// 스탭 로그인 시, 세션 아이디 값을 meberId값에 넣어준다.
-		StaffEntity staffEntity = staffService.update(beforeStaffLogin, httpSession.getId());
+		// 스탭 로그인 시, 쿠키 값을 memberId에 넣어준다.
+		String anonymousMemberId = CookieUtil.getCookieValue(request, CookieUtil.ANONYMOUS_MEMBER_COOKIE_NAME);
+
+		StaffEntity staffEntity = staffService.update(beforeStaffLogin, anonymousMemberId);
 
 		return Responses.ok("성공적으로 스탭의 정보를 가져왔습니다.", toStaffResponse(staffEntity));
 	}
