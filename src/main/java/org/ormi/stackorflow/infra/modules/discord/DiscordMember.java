7e7a74a4889f53e4390e00068c622336c7279fba
devdynam0507 <wsnam0507@naver.com>
2024-06-25 20:17:18 +0900

    feat: 디스코드 유저 avatar url 데이터 추가

--- src/main/java/org/ormi/stackorflow/infra/modules/discord/DiscordMember.java
+++ src/main/java/org/ormi/stackorflow/infra/modules/discord/DiscordMember.java
@@ -6,27 +6,31 @@ import discord4j.core.object.entity.User;
 
 public record DiscordMember(
   long id,
-  String nickname
+  String nickname,
+  String profileImageUrl
 ) {
 
   public static DiscordMember from(Member member) {
     return new DiscordMember(
         member.getUserData().id().asLong(),
-        member.getNickname().orElse("")
+        member.getNickname().orElse(""),
+        member.getAvatarUrl()
     );
   }
 
   public static DiscordMember from(User user) {
     return new DiscordMember(
         user.getId().asLong(),
-        user.getGlobalName().orElse("")
+        user.getGlobalName().orElse(""),
+        user.getAvatarUrl()
     );
   }
 
   public static DiscordMember from(PartialMember partialMember) {
     return new DiscordMember(
         partialMember.getId().asLong(),
-        partialMember.getMemberData().nick().get().orElse("")
+        partialMember.getMemberData().nick().get().orElse(""),
+        partialMember.getAvatarUrl()
     );
   }
 }
